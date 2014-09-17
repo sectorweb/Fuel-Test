189a0a1ee09d33c495a4cd9061fc6c528328edf3
sectorweb <omar@sectorweb.mx>
2014-09-16 19:34:58 -0500

    Load FuelPH

---
+++ fuel/app/logs/2014/09/16.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2014-09-16 19:27:43 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
