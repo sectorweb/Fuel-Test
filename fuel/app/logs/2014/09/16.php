191a6855209f3fa0e0940fd731b4a09d3a9b0009
sectorweb <omar@sectorweb.mx>
2014-09-16 20:21:58 -0500

    Creación del controlador Home

--- fuel/app/logs/2014/09/16.php
+++ fuel/app/logs/2014/09/16.php
@@ -1,3 +1,7 @@
 <?php defined('COREPATH') or exit('No direct script access allowed'); ?>
 
 WARNING - 2014-09-16 19:27:43 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-09-16 19:37:05 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-09-16 20:00:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-09-16 20:00:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2014-09-16 20:21:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
